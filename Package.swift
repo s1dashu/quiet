2b9467700ea70eb778ae69db8b5c59e435ea8fa5
s1dashu <sidashu233@gmail.com>
2026-06-16 04:22:26 +0800

    Add Swift package manifest

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Neat",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "neat", targets: ["PiDesktopWidget"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/JakubMazur/lucide-icons-swift.git", from: "1.19.0"),
+        .package(url: "https://github.com/gonzalezreal/swift-markdown-ui.git", from: "2.4.1")
+    ],
+    targets: [
+        .executableTarget(
+            name: "PiDesktopWidget",
+            dependencies: [
+                .product(name: "LucideIcons", package: "lucide-icons-swift"),
+                .product(name: "MarkdownUI", package: "swift-markdown-ui")
+            ],
+            resources: [
+                .copy("Resources")
+            ]
+        )
+    ]
+)
